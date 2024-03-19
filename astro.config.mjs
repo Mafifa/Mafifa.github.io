40252092273c348b0ed364d9be8e970bf86cae45
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-19 12:10:45 -0400

    🖌Now the Eslint works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import astroI18next from 'astro-i18next'
 
-const LIVE_URL = "http://mafifa.github.io/";
+const LIVE_URL = 'http://mafifa.github.io/'
 
 // https://astro.build/config
 export default defineConfig({
   site: LIVE_URL,
-  integrations: [tailwind(), astroI18next()],
-});
+  integrations: [tailwind(), astroI18next()]
+})
