9684fb06897dfe670c63475e78d83746bb7a4a30
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-19 11:54:09 -0400

    First base before to add eslit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 const LIVE_URL = "http://mafifa.github.io/";
 
 // https://astro.build/config
 export default defineConfig({
   site: LIVE_URL,
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroI18next()],
 });
