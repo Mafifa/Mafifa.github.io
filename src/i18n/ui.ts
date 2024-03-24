3e0bbd5a99d777fffe51eb52f0c2196c47eb7b94
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-24 01:44:51 -0400

    Confing to i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,77 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+  es: 'Español'
+
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: {
+    'header.start': 'Start',
+    'header.experience': 'Experience',
+    'header.projects': 'Projects',
+    'header.about': 'About me',
+    'site.title': "Mafifa's portfolio - Developer and Web programmer",
+    'site.description': 'Mafifa is a Junior web developer and a System Engineer student',
+    'site.im': "Hey, I'm Mafifa",
+    'site.work': 'Available to work',
+    'site.about': 'Systems Engineering student and Junior Fullstack web developer. Specialized in TypeScript, React, Astro and NextJS bases. With experience in project leadership and optimization of digital experiences.',
+    'site.workE': 'Work experience',
+    'site.projects': 'Projects',
+    'site.aboutMe': 'About me',
+    'experience.title': 'Manager - General Billing System Supervisor',
+    'experience.description': 'During the summer of 2023, I assumed the role of manager and leader at a small appliance retailer. My responsibilities included overseeing daily operations and providing leadership to the team. In addition to managerial duties, I actively contributed to the maintenance of the billing system. This system, built on a SQL database foundation and programmed in C++, was a crucial component to the business.',
+    'experience.date': 'June 2023/ October 2023',
+    'projects.description': 'Maffi-type is an application built in ReactJS and TypeScript that uses Tailwind CSS to minimize resource consumption. ReactJS takes care of creating a dynamic user interface, TypeScript ensures code integrity with static types, while Tailwind CSS provides predefined styles that are applied directly to HTML elements, reducing the need to write custom CSS. This combination of technologies enables an efficient and well-styled user experience in Maffi-type.',
+    'projects.repository': 'Repository',
+    'about.p1': "I'm Carlos, but the people call me Mafifa (it's my last name) since I can remember. I love videogames and at the age of 12, I ventured into programming, playing with C++ and the Windows CMD.",
+    'about.p2': "In high school, JavaScript captivated me and later came my eternal love, TypeScript. Today, I'm a junior web programmer focused on React, Astro and Next.js.",
+    'footer.template': 'Template based on that of'
+  },
+  fr: {
+    'header.start': 'Start',
+    'header.experience': 'Experience',
+    'header.projects': 'Projects',
+    'header.about': 'About me',
+    'site.title': "Mafifa's portfolio - Developer and Web programmer",
+    'site.description': 'Mafifa is a Junior web developer and a System Engineer student',
+    'site.im': "Hey, I'm Mafifa",
+    'site.work': 'Available to work',
+    'site.about': 'Systems Engineering student and Junior Fullstack web developer. Specialized in TypeScript, React, Astro and NextJS bases. With experience in project leadership and optimization of digital experiences.',
+    'site.workE': 'Work experience',
+    'site.projects': 'Projects',
+    'site.aboutMe': 'About me',
+    'experience.title': 'Manager - General Billing System Supervisor',
+    'experience.description': 'During the summer of 2023, I assumed the role of manager and leader at a small appliance retailer. My responsibilities included overseeing daily operations and providing leadership to the team. In addition to managerial duties, I actively contributed to the maintenance of the billing system. This system, built on a SQL database foundation and programmed in C++, was a crucial component to the business.',
+    'experience.date': 'June 2023/ October 2023',
+    'projects.description': 'Maffi-type is an application built in ReactJS and TypeScript that uses Tailwind CSS to minimize resource consumption. ReactJS takes care of creating a dynamic user interface, TypeScript ensures code integrity with static types, while Tailwind CSS provides predefined styles that are applied directly to HTML elements, reducing the need to write custom CSS. This combination of technologies enables an efficient and well-styled user experience in Maffi-type.',
+    'projects.repository': 'Repository',
+    'about.p1': "I'm Carlos, but the people call me Mafifa (it's my last name) since I can remember. I love videogames and at the age of 12, I ventured into programming, playing with C++ and the Windows CMD.",
+    'about.p2': "In high school, JavaScript captivated me and later came my eternal love, TypeScript. Today, I'm a junior web programmer focused on React, Astro and Next.js.",
+    'footer.template': 'Template based on that of'
+  },
+  es: {
+    'header.start': 'Inicio',
+    'header.experience': 'Experiencia',
+    'header.projects': 'Proyectos',
+    'header.about': 'Sobre mi',
+    'site.title': 'Mafifa portafolio - Desarrolador y Programador Web',
+    'site.description': 'Mafifa es un Desarrollador Web junior y un estudiante de Ingieneria en Sistemas',
+    'site.im': 'Hey, Soy Mafifa',
+    'site.work': 'Disponible',
+    'site.about': 'Estudiante de Ingeniería de Sistemas y desarrollador web Junior Fullstack. Especializado en bases TypeScript, React, Astro y NextJS. Con experiencia en liderazgo de proyectos y optimización de experiencias digitales.',
+    'site.workE': 'Experiencia laboral',
+    'site.projects': 'Proyectos',
+    'site.aboutMe': 'Sobre mi',
+    'experience.title': 'Gerente - Supervisor del Sistema General de Facturación',
+    'experience.description': 'Durante el verano de 2023, asumí el papel de gerente y líder en un pequeño minorista de electrodomésticos. Mis responsabilidades incluían supervisar las operaciones diarias y proporcionar liderazgo al equipo. Además de las tareas de gestión, contribuí activamente al mantenimiento del sistema de facturación. Este sistema, construido sobre una base de datos SQL y programado en C++, era un componente crucial para el negocio.',
+    'experience.date': 'Junio 2023/ Octubre 2023',
+    'projects.description': 'Maffi-type es una aplicación construida en ReactJS y TypeScript que utiliza Tailwind CSS para minimizar el consumo de recursos. ReactJS se encarga de crear una interfaz de usuario dinámica, TypeScript garantiza la integridad del código con tipos estáticos, mientras que Tailwind CSS proporciona estilos predefinidos que se aplican directamente a los elementos HTML, lo que reduce la necesidad de escribir CSS personalizado. Esta combinación de tecnologías permite una experiencia de usuario eficiente y con buen estilo en Maffi-type.',
+    'projects.repository': 'Repositorio',
+    'about.p1': 'Soy Carlos, pero la gente me llama Mafifa (es mi apellido) desde que tengo uso de razón. Me encantan los videojuegos y a los 12 años me aventuré en la programación, jugando con C++ y el CMD de Windows.',
+    'about.p2': 'En el instituto, JavaScript me cautivó y más tarde llegó mi amor eterno, TypeScript. Hoy, soy un programador web junior centrado en React, Astro y Next.js.',
+    'footer.template': 'Template basado en el de '
+  }
+} as const
