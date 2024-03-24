9271ac9d46eaf3d5bf5b9048c5f4101c5fcb61de
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-24 01:42:34 -0400

    Add the locales (suport es, fr and en)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import astroI18next from 'astro-i18next'
 
 const LIVE_URL = 'http://mafifa.github.io/'
 
 // https://astro.build/config
 export default defineConfig({
   site: LIVE_URL,
-  integrations: [tailwind(), astroI18next()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['es', 'en', 'fr'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 })
