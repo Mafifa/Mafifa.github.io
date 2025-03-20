a4636a9a9ac081bfbd92f9b6bbe4d86d05535c2d
Mafifa <charlysmafifa@gmail.com>
2025-03-20 18:53:11 -0400

    trying to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
-const LIVE_URL = 'http://mafifa.github.io/';
-
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+const LIVE_URL = "http://mafifa.github.io/"
 
 // https://astro.build/config
 export default defineConfig({
-  source: '/src',
-  site: LIVE_URL,
-  integrations: [tailwind(), react()],
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['es', 'en', 'fr'],
-    routing: {
-      prefixDefaultLocale: true
-    }
-  }
-});
+	source: "/src",
+	site: LIVE_URL,
+	base: "https://github.com/Mafifa/Mafifa.github.io",
+	integrations: [tailwind(), react()],
+	i18n: {
+		defaultLocale: "en",
+		locales: ["es", "en", "fr"],
+		routing: {
+			prefixDefaultLocale: true,
+		},
+	},
+})
